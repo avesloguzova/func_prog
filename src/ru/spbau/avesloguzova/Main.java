858b0baf0b04d47fc53446cb2d6162e20cd69537
isaev <valery.isaev@gmail.com>
2015-02-22 17:13:35 +0300

    Add plus test

--- src/ru/spbau/avesloguzova/Main.java
+++ src/ru/spbau/avesloguzova/Main.java
@@ -4,10 +4,17 @@ import static ru.spbau.avesloguzova.TermFabric.*;
 public class Main {
 
     public static void main(String[] args) {
+        /*
         Variable x = getVariable("x");
         Variable y = getVariable("y");
         Variable z = getVariable("z");
         Term test = getApplication(getApplication(getLambda(x, getLambda(y, getLambda(z, getApplication(x, y)))), y), z);
+        */
+        Term two = Lam("x", Lam("y", Apps(Var("x"), Apps(Var("x"), Var("y")))));
+        Term three = Lam("x", Lam("y", Apps(Var("x"), Apps(Var("x"), Apps(Var("x"), Var("y"))))));
+        Term suc = Lam("n", Lam("x", Lam("y", Apps(Var("n"), Var("x"), Apps(Var("x"), Var("y"))))));
+        Term plus = Lam("n", Lam("m", Apps(Var("n"), suc, Var("m"))));
+        Term test = Apps(plus, three, three);
         System.out.println(test);
         System.out.println(test.reduce());
 //	    Term I = getLambda(x, x);
--- src/ru/spbau/avesloguzova/TermFabric.java
+++ src/ru/spbau/avesloguzova/TermFabric.java
@@ -4,15 +4,18 @@ package ru.spbau.avesloguzova;
  * Created by av on 2/19/15.
  */
 public class TermFabric {
-    public static Variable getVariable(String name) {
+    public static Variable Var(String name) {
         return new Variable(name);
     }
 
-    public static Lambda getLambda(Variable var, Term term) {
-        return new Lambda(var, term);
+    public static Lambda Lam(String var, Term term) {
+        return new Lambda(Var(var), term);
     }
 
-    public static Application getApplication(Term first, Term secound) {
-        return new Application(first, secound);
+    public static Term Apps(Term first, Term... rest) {
+        for (Term term : rest) {
+            first = new Application(first, term);
+        }
+        return first;
     }
 }
