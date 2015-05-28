49cca5eec6d4bdc1c78735060861213ebfda30ba
avesloguzova <sashickk@gmail.com>
2015-05-28 13:39:48 +0300

    HW07

--- hw07/Complex.hs
+++ hw07/Complex.hs
@@ -5,24 +5,43 @@ module Complex where
 data Complex = Complex { real :: Double, im :: Double } deriving Eq
 
 instance Num Complex where
-    (+) = undefined
-    (*) = undefined
-    fromInteger = undefined
-    negate = undefined
-    abs = undefined
-    
+    (+) (Complex r1 im1) (Complex r2 im2) = Complex (r1 + r2) (im1 + im2)
+    (*) (Complex r1 i1) (Complex r2 i2) = Complex (r1 * r2 - i1 * i2) (r1 * i2 + r2 * i1)
+    fromInteger n = Complex (fromInteger n) 0
+    negate (Complex r im) = Complex (-r) (-im)
+    abs (Complex r im) = Complex (sqrt(r*r+im*im)) 0
     signum _ = error "Complex: signum isn't defined"
 
 instance Fractional Complex where
-    (/) = undefined
-    fromRational = undefined
+    (/) (Complex r1 i1) (Complex r2 i2) = Complex ((r1*r2 + i1*i2) / sqrsum) ((-r1 * i2 + i1*r2) / sqrsum) 
+        where sqrsum = r2*r2 + i2*i2
+    fromRational r = Complex (fromRational r) 0
 
 -- show и read должны работать как описано в тестах в Main.hs
 instance Show Complex where
-    show = undefined
+    --show = undefined
+    show (Complex r im) 
+        | r == 0 = show im ++ " * i"
+        | im > 0 = show r ++ " + " ++ show im ++ " * i"
+        | im < 0 = show r ++ " - " ++ show (abs im) ++ " * i"
+        | otherwise = show r
 
 instance Read Complex where
-    readsPrec = undefined
+    --readsPrec :: Int -> String -> [(a,String)]
+    readsPrec i str = 
+        let (r,str') = parseDouble str 
+            parseDouble s = case (readsPrec i s :: [(Double, String)]) of 
+                [] -> error "Parsing error"
+                (x:xs) -> x 
+        in case splitAt 3 str' of
+            (" + ", str'') -> 
+                let (im, " * i") = parseDouble str'' in [(Complex r im, "")]
+            (" - ", str'') -> 
+                let (im," * i") = parseDouble str'' in [(Complex r (-im),"")]
+            (" * ", ('i':str'')) -> [(Complex 0 r,str'')]
+            (_, str'') -> [(Complex r 0, str'')]
+
+
 
 i :: Complex
-i = undefined
+i = Complex 0 1
--- hw07/MapLike.hs
+++ hw07/MapLike.hs
@@ -7,12 +7,40 @@ import qualified Data.List as L
 -- 1. Определить класс MapLike типов, похожих на Map.
 --    В нем должны быть функции empty, lookup, insert, delete, fromList с типами как в Data.Map.
 --    Напишите реализацию по умолчанию для fromList.
+class MapLike m where
+	empty :: m k v
+	lookup :: Ord k => k -> m k v -> Maybe v
+	insert :: Ord k => k -> v -> m k v -> m k v
+	delete :: Ord k => k -> m k v -> m k v
+	fromList :: Ord k => [(k, v)] -> m k v
+	fromList [] = empty
+	fromList ((key,value):xs) = insert key value (fromList xs)
 
 -- 2. Определить instance MapLike для Data.Map, ListMap и ArrMap
 --    Можно использовать любые стандартные функции.
 
 newtype ListMap k v = ListMap [(k,v)]
+instance MapLike ListMap where
+	empty = ListMap []
+	lookup _ (ListMap []) = Nothing
+	lookup k (ListMap ((key,value):xs)) = if k == key then Just value
+	                                      else lookup k (ListMap xs)
+	insert key value (ListMap xs) = ListMap $ insert' xs where
+		insert' [] = [(key,value)]
+		insert' (y:ys) = if key == (fst y) then (key,value):ys
+	                     else  y:(insert' ys)
+	delete key (ListMap xs) = ListMap $ filter (\(k,v) -> k /= key) xs
 
 newtype ArrMap k v = ArrMap (k -> Maybe v)
+instance MapLike ArrMap where
+	empty = ArrMap $ \_ -> Nothing
+	insert key value (ArrMap fn) = 
+		ArrMap $ \x -> if key == x then Just value else fn x
+	lookup key (ArrMap fn) = fn key
+	delete key (ArrMap fn) = ArrMap $ \x -> if key == x then Nothing else fn x
 
 -- 3. Написать instace Functor для ListMap k и ArrMap k.
+instance Functor (ListMap k) where
+	fmap fn (ListMap xs) = ListMap $ map (\(key,val) -> (key, fn val)) xs 
+instance Functor (ArrMap k) where
+	fmap fn (ArrMap m) = \key -> fmap fn (m key)
